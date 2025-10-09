18705d149afc5f64199356daf890ce06582bb591
Dackohn <plescadenis86@gmail.com>
2025-10-10 02:01:14 +0300

    Added the possibility to choose the target directory for the download

--- src/client.py
+++ src/client.py
@@ -4,14 +4,17 @@ import os
 
 def main():
     if len(sys.argv) < 4:
-        print("Usage: python client.py <server_host> <server_port> <filename>")
+        print("Usage: python client.py <server_host> <server_port> <filename> [subdir]")
         return
 
     host = sys.argv[1]
     port = int(sys.argv[2])
     filename = sys.argv[3]
+    subdir = sys.argv[4] if len(sys.argv) > 4 else ""
 
+    download_dir = os.path.join("contents", subdir)
     request = f"GET /{filename} HTTP/1.0\r\nHost: {host}\r\n\r\n"
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
     s.sendall(request.encode("utf-8"))
@@ -26,12 +29,18 @@ def main():
 
     header, _, body = response.partition(b"\r\n\r\n")
 
-    os.makedirs("contents/downloads", exist_ok=True)
-    output_path = os.path.join("contents/downloads", os.path.basename(filename))
+    if filename.lower().endswith(".html"):
+        print(body.decode("utf-8", errors="replace"))
+        return
+    print("CWD:", os.getcwd())
+    print("download_dir:", download_dir)
+
+    os.makedirs(download_dir, exist_ok=True)
+    output_path = os.path.join(download_dir, os.path.basename(filename))
     with open(output_path, "wb") as f:
         f.write(body)
 
-    print(f"Downloaded: {output_path}")
+    print(f"Downloaded: {output_path}, download_di: {download_dir}")
 
 if __name__ == "__main__":
     main()
